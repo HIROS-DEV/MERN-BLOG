629731b061e7eff968bc0a95ce042e009acb4754
HIROS-DEV <hirosdev@gmail.com>
2022-01-01 12:39:07 +0900

    added heroku url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,3 +1,5 @@
-const baseURL = 'http://localhost:5000/api';
+// const baseURL = 'http://localhost:5000/api';
+const baseURL =
+	'https://hiros-dev-mern-blog.herokuapp.com/api';
 
 export default baseURL;
